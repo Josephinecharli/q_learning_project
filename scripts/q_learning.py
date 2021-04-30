3aecbdea0f0e7e4440d00f432352ebc86400808b
Evan Michael Casey <ecasey@farfetchd.cs.uchicago.edu>
2021-04-30 15:57:41 -0500

    updated q_learning.py

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -3,6 +3,10 @@
 import rospy
 import numpy as np
 import os
+import randomi
+from q_learning_project.msg import QMatrix
+from q_learning_project.msg import QLearningReward
+from q_learning_project.msg import RobotMoveDBToBlock
 
 # Path of directory on where this file is located
 path_prefix = os.path.dirname(__file__) + "/action_states/"
@@ -43,6 +47,40 @@ class QLearning(object):
         # Note: that not all states are possible to get to.
         self.states = np.loadtxt(path_prefix + "states.txt")
         self.states = list(map(lambda x: list(map(lambda y: int(y), x)), self.states))
+        
+        self.qmatrix = [[0]*9]*64
+
+        self.qmatrix_pub = rospy.Publisher("/q_learning/q_matrix", QMatrix, queue_size=10)
+        self.db_pub = rospy.Publisher("q_learning/robot_action", RobotMoveDBToBlock, queue_size = 10)
+        rospy.subscriber("/q_learning/reward", QLearningReward, self.recieved_reward)
+        
+        
+        self.reward = 0
+        self.iterations = 0
+        self.curr_state = 0
+        self.alpha = 1
+        self.gamma = 0.8
+        self.run()
+
+    def recieved_reward(self, data):
+        self.reward = data.reward
+        self.iterations = data.iteration_num
+
+    def run(self):
+        poss_actions  = filter(lambda x : x >= 0, self.action_matrix[self.curr_state])
+        if len(poss_actions) == 0:
+            print("how did we get here")
+            self.save_q_matrix()
+        else:
+            next_action = poss_actions[random.randrange(len(poss_actions))]
+            db_color = self.actions[next_action][0]
+            block_num = self.actions[next_action][1]
+            self.db_pub.publish(RobotMoveDBToBlock(robot_db=db_color, block_id=block_num))
+            
+            next_state = self.action_matrix[0].index(next_action)# TODO: does this actually work?
+
+            self.qmatrix[self.curr_state][next_action] += self.alpha*(self.reward+self.gamma*max(
+
 
     def save_q_matrix(self):
         # TODO: You'll want to save your q_matrix to a file once it is done to
@@ -51,3 +89,4 @@ class QLearning(object):
 
 if __name__ == "__main__":
     node = QLearning()
+    rospy.spin()
